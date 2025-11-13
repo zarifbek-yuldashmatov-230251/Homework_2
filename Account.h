e39d69c4aa4b80a964e53bde93ceed920033fc8f
zarifbek-yuldashmatov-230251 <161605979+zarifbek-yuldashmatov-230251@users.noreply.github.com>
2025-11-13 21:31:31 +0500

    Update Account.h

--- Account.h
+++ Account.h
@@ -8,44 +8,43 @@ class Account {
 protected:
     string ownerName;
     double balance;
-
 public:
     Account(string name = "", double blnc = 0.0) {
         ownerName = name;
         balance = blnc;
     }
-
+    
     double getBalance() const {
         return balance;
     }
-
+    
     virtual void display() const {
         cout << "Owner: " << ownerName << endl;
         cout << "Balance: " << balance << endl;
     }
-
+    
     virtual ~Account() {
         cout << "Account closed for " << ownerName << endl;
     }
-
+    
     Account operator+(const Account &other) const {
         return Account(ownerName, balance + other.balance);
     }
-
+    
     double operator-(const Account &other) const {
         return balance - other.balance;
     }
-
+    
     bool operator==(const Account &other) const {
         return balance == other.balance;
     }
-
+    
     friend ostream &operator<<(ostream &out, const Account &a) {
         out << "Owner: " << a.ownerName << endl;
         out << "Balance: " << a.balance << endl;
         return out;
     }
-
+    
     friend istream &operator>>(istream &in, Account &a) {
         in >> a.ownerName >> a.balance;
         return in;
@@ -55,13 +54,12 @@ public:
 class SavingsAccount : public Account {
 private:
     double interestRate;
-
 public:
     SavingsAccount(string n, double b, double intrate)
         : Account(n, b) {
         interestRate = intrate;
     }
-
+    
     void display() const override {
         Account::display();
         cout << "Interest Rate: " << interestRate << "%" << endl;
@@ -71,13 +69,12 @@ public:
 class CheckingAccount : public Account {
 private:
     double transactionFee;
-
 public:
     CheckingAccount(string n, double b, double transfee)
         : Account(n, b) {
         transactionFee = transfee;
     }
-
+    
     void display() const override {
         Account::display();
         cout << "Transaction Fee: " << transactionFee << endl;
