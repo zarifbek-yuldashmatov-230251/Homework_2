8d03e8e2d4f964a5b893ec7a7bfdd2f8aa2b1de5
zarifbek-yuldashmatov-230251 <161605979+zarifbek-yuldashmatov-230251@users.noreply.github.com>
2025-11-13 17:46:32 +0500

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,25 +1,53 @@
-#ifndef CAR_H
-#define CAR_H
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include <iostream>
+#include <string>
+using namespace std;
 
-#include "Vehicle.h"
-
-class Car : public Vehicle {
-private:
-    int doors;
-
+class Account {
+protected:
+string ownerName;
+double balance;
 public:
-    Car(string b, int y, int d) : Vehicle(b, y) {
-        doors = d;
-    }
-
-    void displayInfo() override {
-        Vehicle::displayInfo();
-        cout << "Doors: " << doors << endl;
-    }
-
-    ~Car() override {
-        cout << "Car is destroyed" << endl;
-    }
+Account (string name, double blnc) {
+ownerName=name;
+balance=blnc;
+}
+double getBalance() {
+return balance;
+}
+virtual void display() {
+cout<<"Owner: "<<ownerName<<endl;
+cout<<"Balance: "<<balance<<endl;
+}
+virtual ~Account () {
+    cout<<"Account closed for "<<ownername<<endl;
+}
 };
 
+class SavingsAccount: public Account {
+private:
+double interestRate;
+public: 
+SavingsAccount (string n, double b, double intrate):Account (string ownername, double balance){
+interestRate=intrate;
+}
+void display() override {
+    Account.display();
+cout<<"Interest Rate: "<<interestRate<<"%"<<endl;
+}
+};
+
+class CheckingAccount: public Account {
+private:
+double transactionFee;
+public: 
+CheckingAccount (string n, double b, double transfee):Account (string ownername, double balance){
+transactionFee=transfee;
+}
+void display() {
+Account.display();
+    cout<<"Transaction Fee: " <<transactionFee<<endl;
+}
+};
 #endif
